013368ed3e0ffc510a48b2b3892909c44fd8120b
lukem <@>
2025-11-19 11:47:03 -0800

    Modified the PokemonSelection screen to include the database. Added to the database so that you can now randomly select three pokemon from the database.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 import './Navbar.css'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom'
 
 function Navbar({navbartoken}) {
     return (
