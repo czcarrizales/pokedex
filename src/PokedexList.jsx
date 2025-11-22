d8b816da91a48b75a4175be145302402fc5dc986
Chazz Carrizales <czcarrizales@yahoo.com>
2025-11-21 20:18:25 -0800

    modal and overlay

--- src/PokedexList.jsx
+++ src/PokedexList.jsx
@@ -1,36 +1,72 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './PokedexList.css'
+import PokemonDetails from './PokemonDetails'
 
 function PokedexList() {
 
     const [pokemon, setPokemon] = useState([])
+    const [selectedPokemondId, setSelectedPokemonId] = useState(null)
+    const [pokemonDetails, setPokemonDetails] = useState(null)
+    const [pokemonSpecies, setPokemonSpecies] = useState(null)
 
     useEffect(() => {
-            const userId = sessionStorage.getItem("userId");
-            fetch(`http://localhost:5000/pokedex?userId=${userId}`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    setPokemon(data)
-                })
+        const userId = sessionStorage.getItem("userId");
+        fetch(`http://localhost:5000/pokedex?userId=${userId}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setPokemon(data)
+            })
 
-            console.log(pokemon)
-        }, [])
+        console.log(pokemon)
+    }, [])
+
+    useEffect(() => {
+        if (!selectedPokemondId) {
+            setPokemonDetails(null)
+            return
+        }
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemondId}`)
+            .then(res => res.json())
+            .then(json => setPokemonDetails(json));
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${selectedPokemondId}`)
+            .then(res => res.json())
+            .then(json => setPokemonSpecies(json));
+    }, [selectedPokemondId])
 
     return (
         <>
             <div className='pokedex-list-main'>
+                <div>
+                    {
+                        pokemon.map((p) => {
+                            return (
+                                <button key={p.id} className='pokemon-box' onClick={() => setSelectedPokemonId(p.id)}>
+                                    <p className='pokemon-number'>{p.id}</p>
+                                    <img id='pokedex-sprite' src={`https://play.pokemonshowdown.com/sprites/ani/${p.name}.gif`} />
+                                    <p>{p.name}</p>
+                                </button>
+                            )
+                        })
+                    }
+                </div>
                 {
-                    pokemon.map((p) => {
-                        return (
-                            <Link key={p.id} to={`/pokemon-details/${p.id}`} className='pokemon-box'>
-                                <p className='pokemon-number'>{p.id}</p>
-                                <img id='pokedex-sprite' src={`https://play.pokemonshowdown.com/sprites/ani/${p.name}.gif`} />
-                                <p>{p.name}</p>
-                                </Link>
-                        )
-                    })
+                    selectedPokemondId && (
+                        <div className="pokemon-overlay">
+                            <div
+                                className="pokemon-modal-shell"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                
+
+                                {pokemonDetails && (
+                                    <PokemonDetails pokemonDetails={pokemonDetails} pokemonSpecies={pokemonSpecies} />
+                                )}
+                            </div>
+                        </div>
+                    )
                 }
             </div>
 
