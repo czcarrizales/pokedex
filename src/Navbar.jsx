34ad1eec5de81fdc22e50114339084908bd09488
Chazz Carrizales <czcarrizales@yahoo.com>
2025-11-24 13:15:19 -0800

    Logout button in nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 import './Navbar.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function Navbar({navbartoken}) {
+function Navbar({ navbartoken, handleLogout }) {
+
     return (
-     <>
-        <div className='navbar-main'>
-            <Link to="/pokedex" className='nav-link'>Pokedex</Link>
-            <Link to="/pokemon-quiz" className='nav-link'>Game Corner</Link>
-            <Link to="/pokemon-selection" className='nav-link'>Mystery Pull</Link>
-            <Link to="/profile" className='nav-link'>Profile</Link>
-            <button className='navbar-button'>
-                <Link to="/login">Login</Link>
-            </button>
-        </div>
-        <div className = 'token'>
-            <span className = 'count'> x{navbartoken} </span>
-            <img src = "assets/Project3Token.png" alt = "token" className = 'tokenimage'/>
-        </div>
-     </>
+        <>
+            <div className='navbar-main'>
+                <Link to="/pokedex" className='nav-link'>Pokedex</Link>
+                <Link to="/pokemon-quiz" className='nav-link'>Game Corner</Link>
+                <Link to="/pokemon-selection" className='nav-link'>Mystery Pull</Link>
+                <Link to="/profile" className='nav-link'>Profile</Link>
+                <button className='navbar-button' onClick={handleLogout}>
+                    Logout
+                </button>
+            </div>
+            <div className='token'>
+                <span className='count'> x{navbartoken} </span>
+                <img src="assets/Project3Token.png" alt="token" className='tokenimage' />
+            </div>
+        </>
     )
 }
 
