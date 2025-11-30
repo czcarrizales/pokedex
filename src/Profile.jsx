cdf2153421ef28703ccdcad5a30a51a0d87fe73b
FantasticPilot <dantecarrizales2@gmail.com>
2025-11-29 18:30:31 -0800

    Created functional change username button

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,11 @@
-import Navbar from "./Navbar";
 import Tracker from "./Tracker";
 import Graph from "./Graph";
+import SetHometown from "./SetHometown";
+import ChangeUsername from "./ChangeUsername";
 import "./styles/profile.css";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 function Profile() {
-
-    const navigate = useNavigate();
 
     const [userName, setUserName] = useState("");
 
@@ -15,30 +13,52 @@ function Profile() {
         setUserName(sessionStorage.getItem("userName"));
     }, []);
 
+// Old button
+/*
     const handleLogout = async () => {
         sessionStorage.removeItem("userName");
         sessionStorage.removeItem("userId");
         navigate("/login");
     };
+*/
+
+    const [homeVisible, setHomeVisible] = useState(false);
+
+    const handleSetHometown = () => {
+        setHomeVisible(prevState => !prevState);
+    };
+
+    const [homeTown, setHomeTown] = useState("Pallet Town");
+    const [region, setRegion] = useState("Kanto Region");
+
+    const [changeVisible, setChangeVisible] = useState(false);
+    const handleSetChangeVisible = () => {
+        setChangeVisible(prevState => !prevState);
+    };
 
     return (
         <div className="profileContainer">
-            <h1 style={{ marginBottom: "30px" }}>My Profile</h1>
+            <h1 style={{ marginBottom: "30px", marginTop: "30px" }}>My Profile</h1>
             <br />
             <div className="profileBanner">
                 <img className="profileIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png" />
                 <div className="profileName">
                     <p style={{ color: "#DC143C", fontWeight: "bold", fontSize: "20px" }}>{userName}</p>
                     <p>Trainer</p>
-                    <p>Pallet Town,</p>
-                    <p>Kanto Region</p>
+                    <p>{homeTown},</p>
+                    <p>{region}</p>
                 </div>
                 <div className="profileButtonContainer">
-                    <button className="profileButton">Change Username</button>
+                    <button className="profileButton" onClick={handleSetChangeVisible}>{changeVisible ? "Cancel" : "Change Username"}</button>
                     <button className="profileButton">Reset Password</button>
-                    <button className="profileButton"  onClick={handleLogout}>Log Out</button>
+                    <button className="profileButton" onClick={handleSetHometown}>{homeVisible ? "Cancel" : "Set Hometown"}</button>
                 </div>
             </div>
+            {changeVisible && <ChangeUsername />}
+            {homeVisible && <SetHometown onSelectTown={(town, regionName) => {
+                setHomeTown(town);
+                setRegion(regionName);
+            }} />}
             <div className="activityContainer">
                 <Tracker />
                 <Graph />
